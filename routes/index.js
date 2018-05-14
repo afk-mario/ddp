e4c8bc04fd37b16b4b46aeef86d42d7cca7e6afe
afk <afk@ellugar.co>
2018-05-14 23:09:19 +0100

    new routes and css
    - Added new route for episode-single
    - Improved nojs support
    - Added about route and view
    - Added 404 route and view

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,6 @@ function formatBytes(bytes, decimals) {
   return `${num} ${sizes[i]}`;
 }
 
-// Home page route.
 router.get('/', async (req, res) => {
   try {
     const podcastRequest = await got(API_SINGLE, { json: true });
@@ -60,14 +59,105 @@ router.get('/', async (req, res) => {
     context.title = podcast.title;
     context.description = podcast.small_text;
 
-    res.render('pages/index', context);
+    res.render('pages/home', context);
   } catch (error) {
     console.error(error);
   }
 });
 
-router.get('*', (req, res) => {
-  res.status(404).send('404');
+router.get('/episodio/:episodeSlug', async (req, res) => {
+  try {
+    const podcastRequest = await got(API_SINGLE, { json: true });
+    const listRequest = await got(API_LIST, { json: true });
+    const podcast = podcastRequest.body;
+    const list = listRequest.body.reverse().map(episode =>
+      Object.assign(episode, {
+        md: md.render(episode.text),
+        frmtDateMobile: new Date(episode.dateCreated).toLocaleDateString(
+          'es-mx',
+          {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+          },
+        ),
+        frmtDateDesktop: new Date(episode.dateCreated).toLocaleDateString(
+          'es-mx',
+          {
+            year: '2-digit',
+            month: 'short',
+            day: '2-digit',
+          },
+        ),
+        frmtSize: formatBytes(episode.audio_size),
+      }),
+    );
+    const single = list.find(
+      episode => episode.slug === req.params.episodeSlug,
+    );
+
+    if (!single) {
+      res.status(404);
+
+      // respond with html page
+      if (req.accepts('html')) {
+        const context = { ...DEFAULT_META, podcast, url: req.url };
+        res.render('pages/404', context);
+        return;
+      }
+
+      // respond with json
+      if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+      }
+
+      // default to plain-text. send()
+      res.type('txt').send('Not found');
+      return;
+    }
+
+    const context = { ...DEFAULT_META, podcast, list, single };
+
+    context.title = single.title;
+    context.description = single.small_text;
+    context.preview_url = single.image;
+
+    res.render('pages/episode', context);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get('/acerca-de', async (req, res) => {
+  const podcastRequest = await got(API_SINGLE, { json: true });
+  const podcast = podcastRequest.body;
+  podcast.md = md.render(podcast.text);
+
+  const context = { ...DEFAULT_META, podcast };
+  res.render('pages/about', context);
+});
+
+router.get('*', async (req, res) => {
+  res.status(404);
+  const podcastRequest = await got(API_SINGLE, { json: true });
+  const podcast = podcastRequest.body;
+
+  // respond with html page
+  if (req.accepts('html')) {
+    const context = { ...DEFAULT_META, podcast, url: req.url };
+    res.render('pages/404', context);
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
 });
 
 module.exports = router;
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          DEV ? 'style-loader' : MiniCssExtractPlugin.loader,
+          MiniCssExtractPlugin.loader,
           { loader: 'css-loader', options: { sourceMap: true } },
           { loader: 'postcss-loader', options: { sourceMap: true } },
         ],
