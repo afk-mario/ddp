617ecd1f48fd6fe8cd671ab6b485239822b3098e
afk <afk@ellugar.co>
2018-05-15 17:46:05 +0100

    Added blog and article pages

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,11 @@ const Remarkable = require('remarkable');
 const md = new Remarkable();
 
 const router = express.Router();
-const API_URL = 'https://api.ellugar.co/podcast/json/';
+const API_ROOT = 'https://api.ellugar.co/';
+const API_URL = `${API_ROOT}podcast/json/`;
 const API_SINGLE = `${API_URL}podcast/1/`;
 const API_LIST = `${API_URL}episodes/detras-del-pixel/`;
+const API_BLOG_LIST = `${API_ROOT}diary/posts/?tags__name=detras-del-pixel`;
 const DEFAULT_META = {
   title: 'Detrás del Pixel',
   description: 'Sitio web para detrás del pixel',
@@ -65,7 +67,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/episodio/:episodeSlug', async (req, res) => {
+router.get('/episodio/:slug', async (req, res) => {
   try {
     const podcastRequest = await got(API_SINGLE, { json: true });
     const listRequest = await got(API_LIST, { json: true });
@@ -92,9 +94,7 @@ router.get('/episodio/:episodeSlug', async (req, res) => {
         frmtSize: formatBytes(episode.audio_size),
       }),
     );
-    const single = list.find(
-      episode => episode.slug === req.params.episodeSlug,
-    );
+    const single = list.find(episode => episode.slug === req.params.slug);
 
     if (!single) {
       res.status(404);
@@ -136,6 +136,97 @@ router.get('/acerca-de', async (req, res) => {
 
   const context = { ...DEFAULT_META, podcast };
   res.render('pages/about', context);
+});
+
+router.get('/blog', async (req, res) => {
+  const podcastRequest = await got(API_SINGLE, { json: true });
+  const listRequest = await got(API_BLOG_LIST, { json: true });
+  const podcast = podcastRequest.body;
+  const list = listRequest.body.map(article =>
+    Object.assign(article, {
+      md: md.render(article.text),
+      frmtDateMobile: new Date(article.dateCreated).toLocaleDateString(
+        'es-mx',
+        {
+          year: '2-digit',
+          month: '2-digit',
+          day: '2-digit',
+        },
+      ),
+      frmtDateDesktop: new Date(article.dateCreated).toLocaleDateString(
+        'es-mx',
+        {
+          year: '2-digit',
+          month: 'short',
+          day: '2-digit',
+        },
+      ),
+    }),
+  );
+  podcast.md = md.render(podcast.text);
+
+  const context = { ...DEFAULT_META, podcast, list };
+  res.render('pages/article-list', context);
+});
+
+router.get('/articulo/:slug', async (req, res) => {
+  try {
+    const podcastRequest = await got(API_SINGLE, { json: true });
+    const listRequest = await got(API_BLOG_LIST, { json: true });
+    const podcast = podcastRequest.body;
+    const list = listRequest.body.map(article =>
+      Object.assign(article, {
+        md: md.render(article.text),
+        frmtDateMobile: new Date(article.dateCreated).toLocaleDateString(
+          'es-mx',
+          {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+          },
+        ),
+        frmtDateDesktop: new Date(article.dateCreated).toLocaleDateString(
+          'es-mx',
+          {
+            year: '2-digit',
+            month: 'short',
+            day: '2-digit',
+          },
+        ),
+      }),
+    );
+    podcast.md = md.render(podcast.text);
+    const single = list.find(article => article.slug === req.params.slug);
+
+    if (!single) {
+      res.status(404);
+
+      // respond with html page
+      if (req.accepts('html')) {
+        const context = { ...DEFAULT_META, podcast, url: req.url };
+        res.render('pages/404', context);
+        return;
+      }
+
+      // respond with json
+      if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+      }
+
+      // default to plain-text. send()
+      res.type('txt').send('Not found');
+      return;
+    }
+
+    const context = { ...DEFAULT_META, podcast, list, single };
+
+    context.title = single.title;
+
+    res.render('pages/article-single', context);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 router.get('*', async (req, res) => {
